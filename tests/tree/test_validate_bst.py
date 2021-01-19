4f33b03cf11cbcdebf6df421aa2bda49ede7d0d2
Saurabh Mishra <saurab.mish@gmail.com>
2021-01-18 20:56:50 -0800

    verify inorder traversal which determines whether a binary tree is BST

---
+++ tests/tree/test_validate_bst.py
@@ -0,0 +1,39 @@
+import unittest
+
+from tree.node import BinaryNode
+from tree.validate_bst import validate_bst
+
+class TestValidateBST(unittest.TestCase):
+
+    def test_valid_bst(self):
+        """Binary Tree
+
+                    2
+                   / \
+                  1   5
+        """
+        self.root = BinaryNode(2)
+        self.root.left = BinaryNode(1)
+        self.root.right = BinaryNode(5)
+
+        self.assertTrue(validate_bst(self.root))
+
+
+    def test_invalid_bst(self):
+        """Binary Tree
+
+                      5
+                     / \
+                    /   \
+                   /     \
+                  1       4
+                         / \
+                        3   6
+        """
+        self.root = BinaryNode(5)
+        self.root.left = BinaryNode(1)
+        self.root.right = BinaryNode(4)
+        self.root.right.left = BinaryNode(3)
+        self.root.right.right = BinaryNode(6)
+
+        self.assertFalse(validate_bst(self.root))
